e8f10233535ef3664a7d711ddba5a0956502508d
charlie141 <49026391+charlie141@users.noreply.github.com>
2020-12-02 21:31:47 +0900

    final2

--- final/final/Main.cpp
+++ final/final/Main.cpp
@@ -1,5 +1,7 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 #include "game1.h"
+#include "game2_main.cpp"
 #include "game3.h"
 #include "game4_manager.h"
 #include "game5_minigame.h"
@@ -8,13 +10,18 @@
 
 
 int main()
-{							//로딩화면에 포함될 내용 -> 게임 방법, 스토리 진행 상황 
-	Game1 title;			//매니저가 이 게임 오브젝트를 관리하면서 순서대로 display하는 역할 
-	title.init();			//player를 만들고 각 game의 life에 따라 경험치 계산 -> 매니저
-	title.gLoop(0);			//게임 사이 사이의 로딩 창 만들기-> 매니저
-							//시작, 엔딩 화면 띄우기 -> 매니저
-	system("cls");			//시작화면엔 전체적인 게임시작
-							//엔딩 화면 경험치에 따라 멘트 출력
+{							
+	Game1 title;			
+	title.init();			
+	title.gLoop(0);			
+						
+	system("cls");	
+	
+	MiniGame2 miniGame2 = MiniGame2();
+	miniGame2.start();
+	
+	system("cls");
+
 	Game3 test;
 	test.display();
 
--- game4/oop_joys/character.cpp
+++
@@ -1,9 +0,0 @@
-#include "character.h"
-
-
-Character::Character(string name, string studentNo, string dept, int age) {
-	this->name = name;
-	this->studentNo = studentNo;
-	this->dept = dept;
-	this->age = age;
-}
