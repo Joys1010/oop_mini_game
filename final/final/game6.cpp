0a7a217f0e33d5fbcf3b76ae15870608861abdc0
xxeol2 <71129059+xxeol2@users.noreply.github.com>
2020-12-07 19:00:20 +0900

    Game6 수정

--- final/final/game6.cpp
+++ final/final/game6.cpp
@@ -25,13 +25,9 @@ void GameManager::decreaseLife() {
 void GameManager::increaseLife() {
 	life++;
 }
-//void GameManager::resetLife() {
-//	life = 5;
-//}
 void GameManager::setLife() {
 	life = 0;
 }
-
 int GameManager::getLevel() {
 	return level;
 }
@@ -46,7 +42,6 @@ void Arrow::setAttribute(int x, int y, int type) {
 	this->y = y;
 	this->type = type;
 }
-
 int Arrow::getType() {
 	return type;
 }
@@ -54,7 +49,7 @@ bool Arrow::getState() {
 	return state;
 }
 void Arrow::setState(bool state) {
-	this->state = state;
+	this->state = state;	
 }
 void Arrow::setXY(int x, int y) { // for 커서 XY 설정
 	this->x = x;
@@ -85,7 +80,7 @@ void View::printAnnounce(GameManager gm) {
 }
 
 void View::drawArrow(Arrow ar) {
-	gotoxy(ar.getX() * 2 + 20, ar.getY() + 5);
+	gotoxy(ar.getX() *2 + 20, ar.getY() + 5);
 	switch (ar.getType()) {
 	case 0: //커서
 		SetColor_White;
@@ -125,17 +120,14 @@ void Game6::playGame(GameManager gm, Arrow arrowArr[], int len) {
 			}
 			if (tmp == len - 1) { //다 깼을때
 				if (gm.getLevel() == 1) {
-					// gm.resetLife();
 					gm.increaseLife();
 					gm.levelUp();
 					Arrow arrow2[58] = { Arrow() };
 					for (int i = 0; i < 58; i++) {
 						arrow2[i].setAttribute(flag_x[i], flag_y[i], flag_type[i]);
 						View::drawArrow(arrow2[i]);
-						// arrow2[i].drawArrow();
 					}
 					View::printAnnounce(gm);
-					//gm.printAnnounce();
 					playGame(gm, arrow2, 58);
 				}
 				else {
@@ -165,73 +157,25 @@ void Game6::playGame(GameManager gm, Arrow arrowArr[], int len) {
 				int tmp_x = arrowArr[tmp].getX();
 				int tmp_y = arrowArr[tmp].getY();
 				arrowArr[0].setXY(tmp_x, tmp_y);
-				system("cls");
-				View::printAnnounce(gm);
-				//gm.printAnnounce();
-				for (int i = 0; i < len; i++) {
-					if (arrowArr[i].getState())
-						View::drawArrow(arrowArr[i]);
-					//arrowArr[i].drawArrow();
-				}
-				key = 0;
 			}
-
 			else if (key == 1 || key == 2 || key == 3 || key == 4) { //다른 방향키
-				if (gm.getLife() > 0) {
+				if (gm.getLife() > 0) { //life가 0 이상이면 -> life 감소
 					gm.decreaseLife();
 				}
-				else {
-					gm.setLife();
-				}
-				// gm.decreaseLife();
-				system("cls");
-				View::printAnnounce(gm);
-				//gm.printAnnounce();
-				for (int i = 0; i < len; i++) {
-					if (arrowArr[i].getState())
-						View::drawArrow(arrowArr[i]);
-					//arrowArr[i].drawArrow();
-				}
-				//if (gm.getLife() > 0) { // 목숨 0개 이상
-				//	View::printAnnounce(gm);
-				//	//gm.printAnnounce();
-				//	for (int i = 0; i < len; i++) {
-				//		if (arrowArr[i].getState())
-				//			View::drawArrow(arrowArr[i]);
-				//			//arrowArr[i].drawArrow();
-				//	}
-				//}
-				//else { //목숨 0개
-				//	gm.setLife();
-				//	
-				//}
-				//else { // 목숨 0 개
-				//	gm.resetLife();
-				//	for (int i = 0; i < len; i++) {
-				//		if (gm.getLevel() == 1) {
-				//			arrowArr[i].setAttribute(sw_x[i], sw_y[i], sw_type[i]);
-				//		}
-				//		else {
-				//			arrowArr[i].setAttribute(flag_x[i], flag_y[i], flag_type[i]);
-				//		}
-				//		arrowArr[i].setState(true);
-				//		View::drawArrow(arrowArr[i]);
-				//		//arrowArr[i].drawArrow();
-				//	}
-				//	View::printAnnounce(gm);
-				//	//gm.printAnnounce();
-				//}
-				//gm.printAnnounce();
-				for (int i = 0; i < len; i++) {
-					if (arrowArr[i].getState())
-						View::drawArrow(arrowArr[i]);
-					//arrowArr[i].drawArrow();
-				}
-				key = 0;
 			}
+			system("cls");
+			View::printAnnounce(gm);
+			for (int i = 0; i < len; i++) {
+				if (arrowArr[i].getState())
+					View::drawArrow(arrowArr[i]);
+			}
+			key = 0;
 		}
 	}
 }
+
+
+// 화살표들의 객체를 생성하고, GameManager 객체를 생성하여 game을 play시킴.
 int Game6::game6() {
 	Arrow arrowArr[53] = { Arrow() };
 
@@ -239,14 +183,15 @@ int Game6::game6() {
 		arrowArr[i].setAttribute(sw_x[i], sw_y[i], sw_type[i]);
 		View::drawArrow(arrowArr[i]);
 	}
+
 	GameManager gm = GameManager();
 	View::printAnnounce(gm);
 	playGame(gm, arrowArr, 53);
 	return gm.getLife();
 }
 
-//int main() {
-//	system("mode con cols=100 lines=25 | title 뮤직톡톡"); // 콘솔창 크기 및 제목 설정
-//	Game6 game = Game6();
-//	game.game6();
-//}
+int main() {
+	system("mode con cols=100 lines=25 | title game6"); // 콘솔창 크기 및 제목 설정
+	Game6 game = Game6();
+	game.game6();
+}
--- final/final/game6.h
+++ final/final/game6.h
@@ -23,29 +23,27 @@
 
 void gotoxy(int x, int y);
 
+// 레벨 및 Life를 관리하는 class
 class GameManager {
 private:
-	int level = 1;
-	int life = 5;
-public:
+	int level=1;
+	int life=5;
+public :
 	int getLife();
 	void increaseLife();
 	void decreaseLife();
-	// void resetLife();
 	void setLife();
-	// void printAnnounce();
 	int getLevel();
 	void levelUp();
 };
-
-
+	
+// 화살표 하나하나의 객체를 위한 class ( 정보(좌표, 현재 상황)를 담고있는 class )
 class Arrow {
 private:
 	int x, y, type;
-	bool state = true;
+	bool state=true;
 public:
 	void setAttribute(int x, int y, int type);
-	// void drawArrow();
 	int getType();
 	bool getState();
 	void setState(bool state);
@@ -54,27 +52,27 @@ public:
 	int getY();
 };
 
+// 화면에 display를 위한 class
 class View {
 public:
 	static void printAnnounce(GameManager gm);
 	static void drawArrow(Arrow ar);
 };
 
-
+// 전체 화살표의 좌표 정보를 담고있고, game 실행을 담당하는 class
 class Game6 {
 private:
 	int sw_x[53] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 9, 8, 7, 6, 5, 4, 3, 2, 1, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 11, 11, 12, 12, 13, 13, 14, 15, 15, 16, 17, 17, 18, 19, 19, 20, 20, 21, 21, 21, 22, 22, 22 };
 	int sw_y[53] = { 10, 10,11,11,11,11,11,11,10,9,8,7,6,6,6,6,6,5,4,3,2,1,1,1,1,1,1,2,3,4,5,6,7,8,9,10,11,10,9,8,9,10,11,10,9,8,7,6,5,4,3,2,1 };
 	int sw_type[53] = { 0, 1,2,4,1,3,2,1,4,4,3,2,1,4,2,4,3,2,4,1,1,3,4,3,2,2,3,2,4,1,2,3,4,2,1,1,2,4,2,1,3,3,2,2,4,1,2,3,1,4,2,1,2 };
-
+	
 	int flag_x[58] = { 17, 16,15,14,13,12,11,10,10,9,8,7,6,6,5,4,3,2,1,2,3,4,3,2,2,3,4,4,5,6,7,7,8,9,9,10,11,12,13,13,13,14,14,14,15,16,16,17,17,17,17,17,17,16,16,16,15,15 };
 	int flag_y[58] = { 1, 1, 1, 1, 1,2,3,3,4,4,4,4,4,5,5,5,5,6,7,8,8,9,10,11,12,12,12,11,11,10,10,9,8,8,9,10,10,9,8,7,6,5,4,3,3,3,4,5,6,7,8,9,10,11,12,13,14,15 };
 	int flag_type[58] = { 0,1,3,3,1,2,3,4,1,3,2,3,1,1,2,3,1,3,4,3,1,3,1,3,3,4,1,3,2,4,3,3,2,3,4,3,1,2,3,1,1,4,4,4,2,3,3,1,3,2,3,3,1,2,1,3,3,1 };
-
-	// int* arr[2][3] = { {sw_x, sw_y, sw_type}, {flag_x,flag_y,flag_type} };
 
 public:
 	void playGame(GameManager gm, Arrow arrowArr[], int len);
 	int game6();
 };
+
 #endif
